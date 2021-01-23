fee7e65a9021f7d20d5b4ee9fd7982dcf0151798
Neto322 <42181276+Neto322@users.noreply.github.com>
2021-01-22 21:21:20 -0600

    Commit

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return "Hola,estas en la raiz del dominio";
+});
+
+Route::get('/noticias', function () {
+    return "Hola,estas en noticias";
+});
+
+Route::post("/noticias", function () 
+{
+    return "Esta ruta es para agregar nuevas noticias";
+});
+
+Route::match( ["get","post"] , "/blog" , function () 
+{
+    return "Aqui andamos con get y possttr";
+});
+
+Route::any( "/promociones" , function () 
+{
+    return "Aqui andamos con todos los metodos posibles";
+
+});
+
+Route::get("/usuarios/{id}",function ($id) 
+{
+    return "Aqui se muestra la informacion del usuario: " . $id;
+});
